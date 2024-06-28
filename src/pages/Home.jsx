160434a8b72b9f2f6027a125e0e9561686cbff00
rajan <rajanshahi1267@gmail.com>
2024-06-28 22:32:40 +0545

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Services from "../components/Services/Services";
 import Banner from "../components/Banner/Banner";
 import AppStore from "../components/AppStore/AppStore";
 import Testimonials from "../components/Testimonials/Testimonials";
+import Order from "../daspages/Order";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Order />
       <Services />
       <Banner />
       <AppStore />
