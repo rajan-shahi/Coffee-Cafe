3b679bc3abc1332ecd6fa6cb2293dba476fec886
rajan <rajanshahi1267@gmail.com>
2024-06-30 00:21:19 +0545

    add

--- src/pages/dashboard/Message.jsx
+++ src/pages/dashboard/Message.jsx
@@ -6,43 +6,43 @@ const Message = () => {
       avatar:
         "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
       name: "Liam James",
-
       phone_number: "9866448106",
       location: "Kathmandu Nepla",
       order: "Premium Coffee",
+      date: "2024-06-30",
     },
     {
       avatar: "https://randomuser.me/api/portraits/men/86.jpg",
       name: "Olivia Emma",
-
       phone_number: "9866448106",
       location: "Surkhet Nepal",
       order: "Hot Coffee",
+      date: "2024-06-29",
     },
     {
       avatar: "https://randomuser.me/api/portraits/women/79.jpg",
       name: "William Benjamin",
-
       phone_number: "9866448106",
       location: "Pokhara Nepal",
       order: "Cold Coffee",
+      date: "2024-06-28",
     },
     {
       avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
       name: "Henry Theodore",
-
       phone_number: "9866448106",
       location: "Nepalgunj Nepal",
       order: "Milk Coffee",
+      date: "2024-06-27",
     },
     {
       avatar:
         "https://images.unsplash.com/photo-1439911767590-c724b615299d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
       name: "Amelia Elijah",
-
       phone_number: "9866448106",
       location: "Dang Nepal",
       order: "Hot Coffee",
+      date: "2024-06-26",
     },
   ];
 
@@ -50,7 +50,7 @@ const Message = () => {
     <div className="max-w-screen-xl mx-auto">
       <div className="items-start justify-between md:flex">
         <div className="max-w-lg">
-          <h3 className="text-gray-800  text-xl ">Total messages</h3>
+          <h3 className="text-gray-800 text-xl">Total messages</h3>
           <p className="text-gray-600 text-sm mt-1">Total number of Messages</p>
         </div>
       </div>
@@ -62,7 +62,7 @@ const Message = () => {
               <th className="py-3 px-6">Mobile Number</th>
               <th className="py-3 px-6">Location</th>
               <th className="py-3 px-6">Order</th>
-              <th className="py-3 px-6"></th>
+              <th className="py-3 px-6">Message date</th>
             </tr>
           </thead>
           <tbody className="text-gray-600 divide-y">
@@ -85,17 +85,7 @@ const Message = () => {
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">{item.location}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{item.order}</td>
-                <td className="text-right px-6 whitespace-nowrap">
-                  <a
-                    href="javascript:void(0)"
-                    className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg"
-                  >
-                    Edit
-                  </a>
-                  <button className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
-                    Delete
-                  </button>
-                </td>
+                <td className="px-6 py-4 whitespace-nowrap">{item.date}</td>
               </tr>
             ))}
           </tbody>
